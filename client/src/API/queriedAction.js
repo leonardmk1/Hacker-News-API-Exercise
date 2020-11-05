1aaf5fb56e963da50c4852d3049750c667b44f52
Matthew Leonard <leonard.matthewk@gmail.com>
2020-11-05 16:24:34 -0500

    changing to https

--- client/src/API/queriedAction.js
+++ client/src/API/queriedAction.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const queryResults = (query) => {
-  return axios.get("http://hn.algolia.com/api/v1/search?query=" + query);
+  return axios.get("https://hn.algolia.com/api/v1/search?query=" + query);
 };
